e15d27e5137b5e00c40b1633a717bbfc58d68d38
sara53 <sara.atef001@gmail.com>
2025-05-05 19:00:59 +0300

    finished responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import '../styles/header.css'
 import { NavLink } from 'react-router-dom'
-export function Header() {
-    return (
-        <Navbar data-bs-theme="light" className='border-bottom'>
-            <Container>
-                <Navbar.Brand href="#home" className="logo sectionTitle">رواد الغناء العربى</Navbar.Brand>
-                <Nav>
-                    <NavLink className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'} to="/">الرئيسيه</NavLink>
-                    <NavLink className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'} to="/articles">المدونه</NavLink>
-                    <NavLink className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'} to="/pioneers">الرواد</NavLink>
-                    <NavLink className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'} to="/contact-us">اتصل بنا</NavLink>
-                </Nav>
-                <div></div>
-                {/* <i className="bi bi-search"></i> */}
 
+export function Header() {
+    const [ expanded, setExpanded ] = useState( false );
+
+    return (
+        <Navbar
+            data-bs-theme="light"
+            className='border-bottom'
+            expand="lg"
+            expanded={expanded}
+        >
+            <Container>
+                <Navbar.Brand href="/" className="logo sectionTitle">رواد الغناء العربى</Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={() => setExpanded( expanded ? false : true )}
+                />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink
+                            className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'}
+                            to="/"
+                            onClick={() => setExpanded( false )}
+                        >
+                            الرئيسيه
+                        </NavLink>
+                        <NavLink
+                            className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'}
+                            to="/articles"
+                            onClick={() => setExpanded( false )}
+                        >
+                            المدونه
+                        </NavLink>
+                        <NavLink
+                            className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'}
+                            to="/pioneers"
+                            onClick={() => setExpanded( false )}
+                        >
+                            الرواد
+                        </NavLink>
+                        <NavLink
+                            className={( { isActive } ) => isActive ? 'nav-link isActive' : 'nav-link'}
+                            to="/contact-us"
+                            onClick={() => setExpanded( false )}
+                        >
+                            اتصل بنا
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
